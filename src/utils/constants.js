9394ec56daebe2750bf1e616e85dc773f191170b
Subhi <Subhi@MacBook-Pro.local>
2024-11-16 10:50:37 +0530

    installed all required dependencies

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const coviData ={
+    "India": {
+      "Total": 1000000,
+      "Active": 200000,
+      "Recovered": 750000,
+      "Deaths": 50000,
+      "States": {
+        "Kerala": { "Total": 50000, "Active": 10000, "Recovered": 38000, "Deaths": 2000 },
+        "Tamil Nadu": { "Total": 70000, "Active": 20000, "Recovered": 47000, "Deaths": 3000 }
+      }
+    }
+  }
+  
